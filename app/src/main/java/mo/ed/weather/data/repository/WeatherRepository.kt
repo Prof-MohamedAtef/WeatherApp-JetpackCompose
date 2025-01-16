3a27b356e14078e6fee518e4d769c08f8a7fb33e
Mohamed Atef <moabdallah.c@stc.com.sa>
2025-01-16 18:20:56 +0200

    inherit from WeatherRepositoryInterface.kt

--- app/src/main/java/mo/ed/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/mo/ed/weather/data/repository/WeatherRepository.kt
@@ -2,12 +2,14 @@ package mo.ed.weather.data.repository
 
 import mo.ed.weather.data.model.WeatherResponse
 import mo.ed.weather.data.remote.WeatherApiService
+import mo.ed.weather.domain.repository.WeatherRepositoryInterface
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(
     private val apiService: WeatherApiService
-) {
-    suspend fun fetchWeather(city: String, apiKey: String): WeatherResponse {
+) : WeatherRepositoryInterface {
+    override suspend fun fetchWeather(city: String, apiKey: String): WeatherResponse {
         return apiService.getWeather(city, apiKey)
     }
+
 }
