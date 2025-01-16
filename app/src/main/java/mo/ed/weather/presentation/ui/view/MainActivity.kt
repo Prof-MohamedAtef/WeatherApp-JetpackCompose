347e1e25dc4ef340caa9a22ed9f77b00268061ab
Mohamed Atef <moabdallah.c@stc.com.sa>
2025-01-16 18:22:21 +0200

    Enhancing MainActivity.kt

--- app/src/main/java/mo/ed/weather/presentation/ui/view/MainActivity.kt
+++ app/src/main/java/mo/ed/weather/presentation/ui/view/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import mo.ed.weather.presentation.ui.screen.WeatherScreen
+import mo.ed.weather.presentation.viewmodel.WeatherViewModel
 import mo.ed.weather.ui.theme.MyWeatherAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,12 +24,14 @@ class MainActivity : ComponentActivity() {
             MyWeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     WeatherScreen(
-                        isDarkMode = true,
+                        viewModel = viewModel,
                         onModeChange = {},
-                        weatherState = "sunny"
+                        isDarkMode = true,
                     )
                 }
             }
         }
+
+        viewModel.fetchWeather("Cairo", "ea2f2f3b31cbc318071a99d9a7b5de08")
     }
 }
